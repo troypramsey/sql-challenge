7792b83156beb18b5b3885723bb3e0bc708261b5
Troy Ramsey <troypramsey@gmail.com>
2020-08-19 17:16:04 -0700

    added last 3 required queries

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -19,3 +19,20 @@ JOIN departments ON dept_emp.department = departments.dept_no;
 -- Question 5: List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B." --
 SELECT first_name, last_name, sex FROM employees
 WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+-- Question 6: List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT emp_no, first_name, last_name, departments.dept_name FROM employees
+JOIN dept_emp ON employees.emp_no = dept_emp.employee
+JOIN departments ON dept_emp.department = departments.dept_no
+WHERE dept_name = 'Sales';
+
+-- Question 7: List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name. --
+SELECT emp_no, first_name, last_name, departments.dept_name FROM employees
+JOIN dept_emp ON employees.emp_no = dept_emp.employee
+JOIN departments ON dept_emp.department = departments.dept_no
+WHERE dept_name = 'Sales' OR dept_name='Development';
+
+-- Question 8: In descending order, list the frequency count of employee last names, i.e., how many employees share each last name. --
+SELECT last_name, COUNT(*) FROM employees
+GROUP BY last_name
+ORDER BY COUNT(*) DESC;
